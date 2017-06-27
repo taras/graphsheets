cec391788a9f45265239fe1cfe6d81d8eac34b02
Taras Mankovski <tarasm@gmail.com>
2017-06-26 21:14:59 -0400

    Refactored to use command layout from preact-cli

--- src/commands/authorize.ts
+++ src/commands/authorize.ts
@@ -1,66 +1,83 @@
 import fs = require('fs-extra');
 import path = require('path');
 
-import { CLIOptions } from '../cli';
 import GoogleAuthorizer from '../adapters/google-auth';
 import { ask } from '../adapters/prompt';
-import Logger from '../adapters/logger';
+import asyncCommand from '../lib/async-command';
 
-export interface AuthorizeOptions extends CLIOptions {
-  readOnly: string
-  tokenPath: string
-  clientSecretPath: string
-}
+export default asyncCommand({
+  command: 'authorize',
+  desc: 'fetch a token to access Google services',
+  builder: {
+    'read-only': {
+      describe: 'gain read only access to Google Sheets',
+      default: false,
+      alias: 'r'
+    },
+    'token-path': {
+      describe: 'path where your token will be saved',
+      default: path.join(process.env.HOME, '.credentials', 'sheets.googleapis.com-sheetsql.json'),
+      alias: 't'
+    },
+    'client-secret-path': {
+      describe: 'path where client_secret.json is located',
+      default: path.join(process.cwd(), 'client_secret.json'),
+      alias: 'c'
+    }
+  },
+  async handler(argv) {
+    let {
+        readOnly,
+        tokenPath,
+        clientSecretPath,
+        verbose
+      } = argv;
 
-export default async function(options: AuthorizeOptions) {
-  let { 
-    tokenPath,
-    clientSecretPath,
-    readOnly
-  } = options;
-
-  let credentials;
-  try {
-    credentials = JSON.parse(await fs.readFile(clientSecretPath, "utf8"));
-  } catch (e) {
-    console.error(`Could not read client_secret.json at ${clientSecretPath}`);
-    throw e;
-  }
-
-  let authorizer = new GoogleAuthorizer(credentials);
-
-  let token;
-  try {
-    token = JSON.parse(fs.readFileSync(tokenPath, "utf8"));
-  } catch (e) {
-    console.error(`Count not read token file at ${tokenPath}`);
-  }
-
-  if (!token) {
-    let scope = getScope(readOnly);
-    let authUrl = authorizer.generateAuthUrl(scope);
-
-    console.info(`Authorize this app going to: ${authUrl}`);
-    let code = await ask('Enter the code from that page here: ');
-
+    let credentials;
     try {
-      token = await authorizer.getToken(code);
+      credentials = JSON.parse(await fs.readFile(clientSecretPath, "utf8"));
     } catch (e) {
-      console.error(`Count not fetch token with code: ${code}`);
+      console.error(`Could not read client_secret.json at ${clientSecretPath}`);
       throw e;
     }
 
+    let authorizer = new GoogleAuthorizer(credentials);
+
+    let token;
     try {
-      storeToken(tokenPath, token);
+      token = JSON.parse(fs.readFileSync(tokenPath, "utf8"));
+    } catch (e) {
+      console.error(`Count not read token file at ${tokenPath}`);
+    }
+
+    if (token) {
       console.info(`Retrieved a token and stored it in ${tokenPath}`);
-    } catch (e) {
-      console.error(`Could not write token to ${tokenPath}`);
-      throw e;
-    }
-  }
+    } else {
+      let scope = getScope(readOnly);
+      let authUrl = authorizer.generateAuthUrl(scope);
 
-  console.info('👍  authorize command was successful.');
-}
+      console.info(`Authorize this app going to: ${authUrl}`);
+      let code = await ask('Enter the code from that page here: ');
+
+      try {
+        token = await authorizer.getToken(code);
+      } catch (e) {
+        console.error(`Count not fetch token with code: ${code}`);
+        throw e;
+      }
+
+      try {
+        storeToken(tokenPath, token);
+        console.info(`Saved token to ${tokenPath}`);
+      } catch (e) {
+        console.error(`Could not write token to ${tokenPath}`);
+        throw e;
+      }
+    }
+
+    console.info('👍  authorize command was successful.');
+  }
+});
 
 function getScope(readOnly): [string] {
   let base = 'https://www.googleapis.com/auth/spreadsheets';
