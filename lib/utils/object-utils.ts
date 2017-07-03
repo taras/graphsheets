d8b1c02779e5a827046ce7d73ec8767426899bd6
Taras Mankovski <tarasm@gmail.com>
2017-07-03 09:56:10 -0400

    Added filterObject and tests for object-utils

--- lib/utils/object-utils.ts
+++ lib/utils/object-utils.ts
@@ -1,31 +1,70 @@
-const { assign } = Object;
+const { assign, keys } = Object;
 
 /**
- * Maps over the keys of an object converting the values of those keys into new
- * objects. The return value will be an object with the same set of
- * keys, but a different set of values. E.g.
+ * Maps over the keys of an object converting the values of 
+ * those keys into new objects. The return value will be an object 
+ * with the same set of keys, but a different set of values. E.g.
  *
- * > mapObject({first: 1, second: 2}, (value)=> value *2)
+ * > mapObject({first: 1, second: 2}, (key, value)=> value *2)
  *
  *   {first: 2, second: 4}
  */
-export function mapObject(object = {}, fn) {
+export function mapObject(object: {}, fn: (key: string, value: any) => any) {
   return reduceObject(object, function(result, name, value) {
     return assign(result, { [name]: fn(name, value) });
   });
 }
 
-export function reduceObject(object, fn, result = {}) {
+/**
+ * Create a new hash with keys and their corresponding values 
+ * removed based on result of callback operation. E.g.
+ * 
+ * filterObject({ small: 1, smallish: 2, big: 4}, (key, value) => value < 3 })
+ *  
+ *  { small: 1, smallish: 2 } 
+ * 
+ * @param object 
+ * @param fn 
+ */
+export function filterObject(object: {}, fn: (key, value) => boolean) {
+  return reduceObject(
+    object,
+    (result, key, value) => {
+      if (fn(key, value)) {
+        return assign(result, {
+          [key]: value
+        });
+      }
+      return result;
+    },
+    {}
+  );
+}
+
+/**
+ * Create a new hash with 
+ * @param object 
+ * @param fn 
+ * @param result 
+ */
+export function reduceObject(
+  object: {},
+  fn: (result: any, key: string, value: any) => any,
+  result: any = {}
+) {
   eachProperty(object, function(name, value) {
     result = fn(result, name, value);
   });
   return result;
 }
 
-export function eachProperty(object, fn) {
+export function eachProperty(
+  object: {},
+  fn: (key: string, value: any) => any
+): void {
   if (typeof object === "object") {
-    Object.keys(object).forEach(function(name) {
-      fn(name, object[name]);
+    keys(object).forEach(function(key) {
+      fn(key, object[key]);
     });
   }
 }
---
+++ test/test.object-utils.ts
@@ -0,0 +1,134 @@
+import * as assert from "power-assert";
+import * as jest from "jest-mock";
+import {
+  mapObject,
+  filterObject,
+  reduceObject
+} from "../lib/utils/object-utils";
+
+describe("mapObject", () => {
+  const original = { first: 1, second: 2 };
+
+  describe("callback use", () => {
+    beforeEach(() => {
+      this.callback = jest.fn();
+      this.result = mapObject(original, this.callback);
+    });
+
+    it("was called twice", () => {
+      assert.equal(this.callback.mock.calls.length, 2);
+    });
+
+    it("was called with key and value", () => {
+      assert.deepEqual(this.callback.mock.calls[0], ["first", 1]);
+      assert.deepEqual(this.callback.mock.calls[1], ["second", 2]);
+    });
+  });
+
+  beforeEach(() => {
+    this.result = mapObject(original, (key, value) => value * 2);
+  });
+
+  it("returns a new object", () => {
+    assert.notEqual(original, this.result);
+  });
+
+  it("returns a hash with result of the callback", () => {
+    assert.deepEqual(this.result, { first: 2, second: 4 });
+  });
+});
+
+describe("filterObject", () => {
+  let original = { small: 1, smallish: 2, big: 4 };
+
+  describe("callback use", () => {
+    beforeEach(() => {
+      this.callback = jest
+        .fn()
+        .mockReturnValueOnce(false)
+        .mockReturnValueOnce(true)
+        .mockReturnValueOnce(undefined);
+      this.result = filterObject(original, this.callback);
+    });
+    it("was called three times", () => {
+      assert.equal(this.callback.mock.calls.length, 3);
+    });
+    it("removed items that returned false", () => {
+      assert.deepEqual(this.result, { smallish: 2 });
+    });
+    it("passed key and value", () => {
+      assert.deepEqual(this.callback.mock.calls[0], ["small", 1]);
+      assert.deepEqual(this.callback.mock.calls[1], ["smallish", 2]);
+      assert.deepEqual(this.callback.mock.calls[2], ["big", 4]);
+    });
+  });
+
+  beforeEach(() => {
+    this.result = filterObject(
+      original,
+      (key, value) => key.indexOf("small") === -1
+    );
+  });
+
+  it("returns a new object", () => {
+    assert.notEqual(this.result, original);
+  });
+
+  it("returns filtered hash", () => {
+    assert.deepEqual(this.result, { big: 4 });
+  });
+});
+
+describe("reduceObject", () => {
+  let original = { small: 1, smallish: 2, big: 4 };
+
+  describe("callback use", () => {
+    beforeEach(() => {
+      this.callback = jest
+        .fn()
+        .mockReturnValueOnce({ SMALL: 10 })
+        .mockReturnValueOnce({ SMALL: 10, SMALLISH: 20 })
+        .mockReturnValueOnce({ SMALL: 10, SMALLISH: 20, BIG: 40 });
+      this.result = reduceObject(original, this.callback);
+    });
+    it("was called three times", () => {
+      assert.equal(this.callback.mock.calls.length, 3);
+    });
+    it("returned the last return value", () => {
+      assert.deepEqual(this.result, { SMALL: 10, SMALLISH: 20, BIG: 40 });
+    });
+    it("was invoked with expected arguments", () => {
+      assert.deepEqual(this.callback.mock.calls[0], [{}, "small", 1]);
+      assert.deepEqual(this.callback.mock.calls[1], [
+        { SMALL: 10 },
+        "smallish",
+        2
+      ]);
+      assert.deepEqual(this.callback.mock.calls[2], [
+        { SMALL: 10, SMALLISH: 20 },
+        "big",
+        4
+      ]);
+    });
+  });
+
+  beforeEach(() => {
+    this.result = reduceObject(original, (result, key, value) => {
+      return Object.assign(result, {
+        [key.toUpperCase()]: value * 10
+      });
+    });
+  });
+
+  it("returned a new object", () => {
+    assert.notEqual(this.result, original);
+  });
+
+  it("returned expected result", () => {
+    assert.deepEqual(this.result, {
+      SMALL: 10,
+      SMALLISH: 20,
+      BIG: 40
+    });
+  });
+});
